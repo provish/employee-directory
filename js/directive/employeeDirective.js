8a484da17458ca273cae199d58744e82a167e673
provish <vshlgupta1992@gmail.com>
2015-08-28 21:12:20 +0530

    updated folder structure

--- js/directive/employeeDirective.js
+++ js/directive/employeeDirective.js
@@ -1,13 +1,17 @@
-
 angular.module('directory.directives', [])
   .directive('employeeData', function () {
     return {
       scope: {
         item: "=data"
       },
-      restrict: 'AE',
+      restrict: 'E',
       replace: false,
       template: '<div><h3>{{item.firstName}} {{item.lastName}}</h3></br>' +
       '<p>{{item.title}}</p> </div>',
+      link: function (scope, elem, attr) {
+        elem.on('click', function (ev) {
+          elem.css('background-color', 'red !important');
+        })
+      }
     }
   });
